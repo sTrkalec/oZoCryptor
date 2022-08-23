8b42e4a2962f7e96d01dd0c9f2e3ef7a2844f572
Gabriel Miranda <vytorgabriel123v@gmail.com>
2022-08-23 00:46:08 -0300

    new project React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import Coin from './components/Coin';
+import logo from './ozo.png';
 
 function App() {
+
+  const [listOfcoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=52").then((res) => {
+      console.log(res.data)
+      setListOfCoins(res.data.coins)
+    })
+
+  }, []);
+
+  const filteredCoins = listOfcoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLocaleLowerCase())
+  })
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='cryptoHeader'>
+        <div>
+          <img className='logo' src={logo} />
+        </div>
+        <input type="text" placeholder='Bitcoin...' onChange={(event) => {
+          setSearchWord(event.target.value)
+        }}></input>
+      </div>
+      <div className='cryptoDisplay'>
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+              linkWeb={coin.websiteUrl}
+            />
+          )
+        })}
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Coin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Coin({ name, icon, price, symbol, linkWeb }) {
+    return (
+        <div className="coin">
+            <h1>{name}</h1>
+            <img src={icon} />
+            <h3>Price: {price.toFixed(2)}</h3>
+            <h3>Symbol: {symbol}</h3>
+            <a target="_blank" href={linkWeb}><button>Web Site {name}</button></a>
+        </div>
+    )
+}
+
+export default Coin;
